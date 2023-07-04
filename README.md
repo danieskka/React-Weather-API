d53ed10ebb1847be44b49236ea3df439e7a1a3f2
danieskka <daniescalera93@hotmail.com>
2023-07-04 19:48:14 +0200

    add/README

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # React-Weather-API
+
+Hola a tod@s!
+
+Os voy a ir contando paso a paso la funcionalidad de este ejercicio realizado con React!
+
+Teneis 2 opciones para poder visualizar el ejercicio:
+
+1. La mas sencilla es que puedas ver la funcionalidad del proyecto a través del siguiente enlace el cual te llevará al despliegue que realice en Netlify y puedas navegar directamente sin necesidad de instalar nada:
+
+- https://main--funny-blini-f3366e.netlify.app/
+
+2. Si deseas clonarte el repositorio y realizar tu mism@ pruebas e instalartelo en tu ordenador, puedes seguir los siguientes pasos:
+
+Para iniciar el proyecto una vez hayais clonado el repositorio (en caso de que querais probarlo) debereis instalar las dependencias, utilizar el siguiente comando en vuestra terminal:
+
+- npm run install
+
+De esta forma os instalara todo lo necesario para que el proyecto pueda iniciarse!
+
+a continuacion utilizar el siguiente comando para arrancar el proyecto:
+
+- npm run dev
+
+Ahora podreis navegar por el siguiente puerto establecido en vuestro local:
+
+- http://localhost:5173/
+
+FUNCIONALIDAD DEL EJERCICIO:
+
+El proyecto cuenta con 1 vista principal en la cual vereis unos datos meteorologicos de Madrid (lo puse por defecto porque es la ciudad donde me encuentro)
+
+![Alt text](image.png)
+
+Te muestra el tiempo a diferentes horas desde el momento que has hecho la busqueda hasta el dia siguiente
+
+Justo debajo veras un input de texto con un boton, esto es un buscador de ciudades, si pruebas a realizar una busqueda y pulsas el boton justo debajo aparecera el tiempo en la ciudad que hayas buscado a diferentes horas!
+
+![Alt text](image-1.png)
+
+Esta aplicación esta recogiendo datos conectadose directamente a una API del tiempo para mostrarte el tiempo en donde tu quieras :D
+
+Dale un intento!
--- src/components/WeatherList/WeatherList.jsx
+++ src/components/WeatherList/WeatherList.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherItem from "../WeatherList/WeatherItem/WeatherItem";
+import "./WeatherList.css";
+
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const WeatherList = () => {
-  
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState([]);
 
@@ -12,7 +13,9 @@ const WeatherList = () => {
     if (city) {
       const fetchData = async () => {
         try {
-          const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`
+          );
           const data = response.data;
           setWeatherData(data.list);
         } catch (error) {
@@ -29,17 +32,32 @@ const WeatherList = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="city" placeholder="Enter a city" />
-        <button type="submit">Search</button>
+    <div className="weather-list-container">
+      <form onSubmit={handleSubmit} className="weather-list-form">
+        <input
+          type="text"
+          name="city"
+          placeholder="Enter a city"
+          className="weather-list-input"
+        />
+        <button type="submit" className="weather-list-button">
+          Search
+        </button>
       </form>
 
+      {weatherData && (
+        <h2 className="weather-list-location">
+          Location: {weatherData.city && weatherData.city.name}
+        </h2>
+      )}
+
       {weatherData.length > 0 ? (
-        weatherData.map((item, index) => <WeatherItem key={index} item={item} />)
-      ) : 
-      (<p>No weather data available.</p>)
-      }
+        weatherData.slice(0, 5).map((item, index) => (
+          <WeatherItem key={index} item={item} />
+        ))
+      ) : (
+        <p className="weather-list-no-data">No weather data available.</p>
+      )}
     </div>
   );
 };
